80dab7dc3607e8b72caaabf6f5abdd85343a67bb
Denis Borchev <borchev@netube.ru>
2016-04-20 12:03:09 +0300

    add some functions listed in the docs
    WARNING: most of the stuff remains un-tested; TODO: verify against lab
    UCSD, correct params where needed; write some params generator (json lib
    wrapper?)

--- pyucsd.py
+++ pyucsd.py
@@ -55,17 +55,21 @@ def ___APIpreparse___(jj):
 		else: return None
 	else: return None # to do: add logging
 
+####################################
+# TODO: params expander dict->json
+####################################
+	
 #############################################################################################################	
 ## Reporting General
 ## Doesn't work at the moment
 
-def GetReportsAvailable(contextName, contextValue):
+def GetReportsList(contextName, contextValue):
     UCSD_API_OPNAME = "userAPIGetAvailableReports"
     u =  "{param0:\"" + contextName + '",' + 'param1:"' + contextValue + '"}'
     res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
     return res
 
-def ___PrintReportsAvailable___(res):
+def ___PrintGetReportsAvailable___(res):
     for r in res:
 	    print "%s\t%s\t%s\n" % (r[u'reportLabel'],r[u'reportType'],r[u'reportId'])
 
@@ -87,14 +91,14 @@ def GetReportsReportBuilderTemplates():
     # Gets a list of all report templates in the system
     UCSD_API_OPNAME = "userAPIGetTabularReport"
     u = '{param0:"10",param1:"null",param2:"REPORT-BUILDER-TEMPLATES-T63"}'
-    res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
     return res	# {"Id":"templateID","Name":"some name","Description":"some description"}
 
 def GetReportsReportBuilder(templateID):
     # Gets a table of reports generated using selected template	
     UCSD_API_OPNAME = "userAPIGetTabularReport"
     u = '{param0:"654",param1:"'+ templateID + '",param2:"REPORT-BUILDER-TEMPLATES-T63"}'
-    res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
     return res
 
 	
@@ -104,12 +108,74 @@ def GetReportsReportBuilder(templateID):
 def GetCatalogAllUsers():
     UCSD_API_OPNAME = "userAPIGetTabularReport"
     u = '{param0:"10",param1:"",param2:"CATALOG-T40"}'
-    res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
     return res
 
+#############################################################################################################
+## Workflows & Service Requests
+
+def DoWorkflowRun(name="", params = dict()):
+    UCSD_API_OPNAME = "userAPISubmitWorkflowServiceRequest"
+    u = "{param0:\"" + name + '",' + 'param1:{' + ___APIworkflowParamsConstructor___(params) + '}' + ',param2:-1}'
+    res = ___APIcall___(APIOP = UCSD_API_OPNAME, params = u)
+    if res["serviceError"] = null: return res
+    return Null
+
+def DoWorkflowRollback():
+    UCSD_API_OPNAME = "userAPIRollbackWorkflow"
+    u = '{param0:{"list":[{"name":"APIServiceRequestParams","value":{"list":[{"name":"srId","value":' + ID + '}]}]}}'
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
+    return res	
+
+def GetWorkflowList(folder=""):
+    UCSD_API_OPNAME = "userAPIGetWorkflows"
+    u = '{param0:{"list":[{"name":"folderName","value":"' + folder + '"}]}}'
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
+    return res
+	
+def GetWorkflowSteps(ID=0):
+    UCSD_API_OPNAME = "userAPIGetWorkflowSteps"
+    u = '{param0:{"list":[{"name":"APIServiceRequestParams","value":{"list":[{"name":"requestId","value":' + ID + '}]}]}}'
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
+    return res	
+
+def GetWorkflowStatus(ID=0):
+    UCSD_API_OPNAME = "userAPIGetWorkflowStatus"
+    u = '{param0:{"list":[{"name":"APIServiceRequestParams","value":{"list":[{"name":"requestId","value":' + ID + '}]}]}}'
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
+    return res	
+
+WorkflowStatus = {0:"EXECUTION_STATUS_NOT_STARTED", 1:"EXECUTION_STATUS_IN_PROGRESS", 2:"EXECUTION_STATUS_FAILED", 3:"EXECUTION_STATUS_COMPLETED", 4:"EXECUTION_STATUS_COMPLETED_WITH_WARNING", 5:"EXECUTION_STATUS_CANCELLED", 6:"EXECUTION_STATUS_PAUSED", 7:"EXECUTION_STATUS_SKIPPED",}
+
+def GetWorkflowInputs(name="")
+    UCSD_API_OPNAME = "userAPIGetWorkflowInputs"
+    u = '{param0:{"list":[{"name":"workflowName","value":"' + name + '"}]}}'
+    res = ___APIcall___(APIOP = UCSD_API_OPNAME, params = u)
+    if res["serviceError"] = null: return res
+    return Null
+
+def GetWorkflowInputLOV(type=""):
+    UCSD_API_OPNAME = "userAPIGetLOVValues"
+    u = '{param0:{"list":[{"name":"type","value":"' + type + '"}]}}'
+    res = ___APIcall___(APIOP = UCSD_API_OPNAME, params = u)
+    if res["serviceError"] = null: return res
+    return Null
+	
+def GetWorkflowInputTable(type=""):
+    UCSD_API_OPNAME = "userAPIGetLOVValues"
+    u = '{param0:{"list":[{"name":"type","value":"' + type + '"}]}}'
+    res = ___APIcall___(APIOP = UCSD_API_OPNAME, params = u)
+    if res["serviceError"] = null: return res
+    return Null
 
 #############################################################################################################
 ## some VM-related functions
+
+def GetVMList():
+    UCSD_API_OPNAME = "userAPIGetAllVMs"
+    u = ""
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
+    return res
 
 def DoVMaction(action, vmid, comstr=""):
     UCSD_API_OPNAME = "userAPIExecuteVMAction"
@@ -130,7 +196,7 @@ def DoVMaction(action, vmid, comstr=""):
     if (not any(action == a for a in generic_actions)): return "Action not valid"
     comments = 'API-VM-ACT-%s: "%s"' % (action, comstr)
     u = "{param0:\"" + vmid + '",' + 'param1:"' + action + '"' + ',param2:"' + comments + '"}'
-    res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
     return res['rows']
 
 def GetVMcompletedActions(vmid):
@@ -139,11 +205,21 @@ def GetVMcompletedActions(vmid):
     param1 = vmid 
     param2 = "VM-ACTION-REQUESTS-T0"
     u = "{param0:\"" + param0 + '",' + 'param1:"' + param1 + '"' + ',param2:"' + param2 + '"}'
-    res = ___APIpreparse___(___APIcall___(APIOP=UCSD_API_OPNAME, params=u))
+    res = ___APIpreparse___(___APIcall___(APIOP = UCSD_API_OPNAME, params = u))
     return res['rows']
 
+	
+#############################################################################################################
+## User functions
+##
+## NOTE: this is important, because many request results are auto-filtered based on user's context, which is
+##       determined by the user name; If I understand the docs correctly, this call is used to impersonate a user:
 
-	
-	
+def GetUserApiKey(user=""):
+    UCSD_API_OPNAME = "userAPIGetRESTAccessKey"
+    u = "{param0:\"" + user + '"}'
+    res = ___APIcall___(APIOP = UCSD_API_OPNAME, params = u)
+    if res["serviceError"] = null: return res
+    return Null
 	
 	
